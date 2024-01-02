1b55926a6c1f35c653fa8f56ae4511e0972fca5c
jannah <s10243581@connect.np.edu.sg>
2024-01-02 14:32:19 +0800

    jannah: firebase changes, added error msg field

--- Assets/Scripts/AuthManager.cs
+++ Assets/Scripts/AuthManager.cs
@@ -8,19 +8,29 @@ using TMPro;
 using Firebase;
 using Firebase.Database;
 using Firebase.Auth;
+using Firebase.Extensions;
 
 public class AuthManager : MonoBehaviour
 {
     Firebase.Auth.FirebaseAuth auth;
 
-    [SerializeField]
-    private TMP_InputField inputEmail;
+    [SerializeField] private TMP_InputField inputEmail;
 
-    [SerializeField]
-    private TMP_InputField inputPassword;
+    [SerializeField] private TMP_InputField inputPassword;
     
-    [SerializeField]
-    private Button btnSignUp; 
+    [SerializeField] private Button btnSignUp; 
+
+    [SerializeField] private TMP_Text errorMsgField;
+    [SerializeField] private string textToDisplay;
+    [SerializeField] private string textToDisplay2;
+
+    public void ChangeErrorMsg()
+    {
+        //errorMsgField.SetActive(true);
+        errorMsgField.text = textToDisplay;
+       
+        Debug.Log("error msg shown");
+    }
 
     public void SignUp()
     {
@@ -38,19 +48,22 @@ public class AuthManager : MonoBehaviour
     {
         //automatically pass user info to the firebase project
         //attempt to create new user or check with there's already one
-        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWith(task =>
+        auth.CreateUserWithEmailAndPasswordAsync(email, password).ContinueWithOnMainThread(task =>
         {
             //perform task handling
             if(task.IsFaulted || task.IsCanceled)
             {
+                ChangeErrorMsg();
                 Debug.LogError("Sorry, there was an error creating your new account, ERROR: " + task.Exception);
+                // error msg
                 return;//exit from the attempt
             }
             else if (task.IsCompleted)
             {
             Firebase.Auth.AuthResult newPlayer = task.Result;
             Debug.LogFormat("Welcome to Piak! Piak! {0}", newPlayer.User.Email);
-            //do anything you want after player creation eg. create new player
+                errorMsgField.text = textToDisplay2;
+                //do anything you want after player creation eg. create new player
             }
         });
 
@@ -62,6 +75,10 @@ public class AuthManager : MonoBehaviour
 
     }
 
+    void Update()
+    {
+        //errorMsg.text = "Error";
+    }
     private void Awake()
     {
         auth = FirebaseAuth.DefaultInstance;
